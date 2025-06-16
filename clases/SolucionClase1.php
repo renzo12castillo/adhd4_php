4403c21cc4a0129f1cb1890ec0b9ac2894a248a5
RenzoLombardoCastillo <renzo12castillo@gmail.com>
2025-06-16 14:13:05 -0600

    Pequena actualizacion agregada, continue vinculando los objetos para que se muestren en pantalla

--- clases/SolucionClase1.php
+++ clases/SolucionClase1.php
@@ -7,7 +7,10 @@
 
         private $resultado; 
 
-        
+        public function promedio () {
+            $this->resultado = ($this->valor1 + $this->valor2 + $this->valor3) / 3;
+            return "El promedio es: " .$this->resultado;
+        }
     }
 
 
--- objetos/solucionObjeto1.php
+++ objetos/solucionObjeto1.php
@@ -1,5 +1,16 @@
 <?php
 
-    require_once("../clases/SolucionClase1.php")
+    require_once("../clases/SolucionClase1.php"); 
+
+    $operacion = new SolucionClase1 ();
+
+    $operacion->valor1 = $_POST['txt_valor_1'];
+    $operacion->valor2 = $_POST['txt_valor_2'];
+    $operacion->valor3 = $_POST['txt_valor_3'];
+
+    if(isset($_POST['btn_calcular'])) {
+        echo "<br>";
+        echo $operacion->promedio(); 
+    }
 
 ?>
